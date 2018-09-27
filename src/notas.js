ed9172f56e8a1066a24829a3e9af8956b2b28feb
AbelMarco <cranchcps@gmail.com>
2018-09-27 20:00:18 +0200

    Servicio de escritura y lectura de todas funcinal

--- src/notas.js
+++ src/notas.js
@@ -1,18 +1,21 @@
 console.log("Modulo de notas")
+const fs = require ('fs')
+let jsonNotes = []
+var stringNotes = fs.readFileSync('./src/notas.json')
+try {
+ jsonNotes = JSON.parse(stringNotes)
+} catch (error) {
 
-const Note = {
-  title:"title",
-  body:"body",
-  Note: (title,body) => {
-    this.title=title
-    this.body=body
-  }
 }
 var devolver
 
 const addNote = (title,body)=> {
-  var nota = Note.Note(title,body)
-  console.log(nota.title)
+  const note = {
+    title,
+    body
+  }
+  jsonNotes.push(note)
+  fs.writeFileSync('./src/notas.json', JSON.stringify(jsonNotes))
   devolver = `He a añadido la nota con titulo: ${title}\ny cuerpo: ${body}`
   return devolver
 }
@@ -25,13 +28,16 @@ const removeNote = (title) => {
 
 const readNote = (title) => {
   
-  devolver = `Voy a leer la nota con titulo: ${title}`
+  devolver = `Te he leido la nota con titulo: ${title}`
   return devolver
 }
 
 const listNotes = () => {
-  
-  devolver = `Voy a leer todas las notas`
+  console.log(jsonNotes)
+  for (var key in jsonNotes) {
+    console.log(`Titulo: ${jsonNotes[key].title}\nCuerpo: ${jsonNotes[key].body}`)
+  }
+  devolver = `Te he leido todas las notas`
   return devolver
 }
 
